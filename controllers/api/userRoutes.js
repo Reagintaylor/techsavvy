dc40a148bf6b6a65a57064142474deabbf046c6d
Reagin Turner <rturnerafteragnes@gmail.com>
2022-01-24 12:43:29 -0500

    Adding the comment routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,16 +1,31 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
-const withAuth = require('../../utils/auth')
+const withAuth = require('../../utils/auth')    // auhtorization
 
 
 // create a user function
 
 router.post('/', async (req, res) => {
   try {
-    
-  }
-})
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+   // making sure the user is logged in
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.loggedIn = true;
 
+      res.status(200).json(newUser);
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// logged in function
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
@@ -18,7 +33,7 @@ router.post('/login', async (req, res) => {
     if (!userData) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect email or password, try again' });
       return;
     }
 
@@ -27,7 +42,7 @@ router.post('/login', async (req, res) => {
     if (!validPassword) {
       res
         .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+        .json({ message: 'Incorrect email or password, try again' });
       return;
     }
 
@@ -43,6 +58,7 @@ router.post('/login', async (req, res) => {
   }
 });
 
+// logging out
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
     req.session.destroy(() => {
@@ -52,5 +68,15 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
+
+// creating a comment, withAuth added to ensure the user is loggedin before they can create a comment
+router.post('/comment', withAuth, async (req, res) => {
+  
+})
+
+
+
+
+
 
 module.exports = router;
