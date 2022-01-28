7ed5f6b6228f1af8a462d939d18fbd9c919d32b1
Reagin Turner <rturnerafteragnes@gmail.com>
2022-01-27 22:03:10 -0500

    Fixed seed/index file

--- seeds/index.js
+++ seeds/index.js
@@ -1,16 +1,21 @@
 const sequelize = require('../config/connection');
 // const { User } = require('../models');
 const userSeed = require('./userData')
-
-const userData = require('./userData');
+const postSeed = require('./postData');
+const commentSeed = require('./commentData');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+  console.log("database synced")
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+  await userSeed();
+  console.log("User seeding complete")
+
+  await postSeed();
+  console.log("Post seeding complete")
+
+  await commentSeed();
+  console.log("Comment seeding complete")
 
   process.exit(0);
 };
